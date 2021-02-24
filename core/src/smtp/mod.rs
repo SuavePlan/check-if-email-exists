b2af6a52ea98d446188019e5a87bc7ed2bbfc05c
SuavePlan <git@suaveplan.com>
2021-02-24 12:10:55 +0000

    Changed default catch-all value to false

--- core/src/smtp/mod.rs
+++ core/src/smtp/mod.rs
@@ -360,7 +360,7 @@ async fn create_smtp_future(
 	// Ok(SmtpDetails { can_connect_smtp: false, ... }).
 	let mut smtp_client = connect_to_host(host, port, input).await?;
 
-	let mut is_catch_all = true;
+	let mut is_catch_all = false;
 
 	if input.disable_catch_all == false {
 		is_catch_all = smtp_is_catch_all(&mut smtp_client, domain)
